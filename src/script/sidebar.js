ad38148e75da281c0eb30b2f945dda092e034a1b
Shovul <140464903+Shovul@users.noreply.github.com>
2024-10-05 16:30:21 +0700

    lam media

--- src/script/sidebar.js
+++ src/script/sidebar.js
@@ -1,18 +1,20 @@
 const sBar = document.getElementById('sidebar')
 
-function toggleSubmenu(button) {
-  button.nextElementSibling.classList.toggle('show');
-  button.classList.toggle('rotate')
-  if (sBar.classList.contains('close')) {
-    sBar.classList.toggle('close');
+if (screen.width > 800) {
+  function toggleSubmenu(button) {
+    button.nextElementSibling.classList.toggle('show');
+    button.classList.toggle('rotate')
+    if (sBar.classList.contains('close')) {
+      sBar.classList.remove('close');
+    }
   }
-}
 
-function toggleSidebar() {
-  sBar.classList.toggle('close');
+  function toggleSidebar() {
+    sBar.classList.toggle('close');
 
-  Array.from(sBar.getElementsByClassName('show')).forEach(ul => {
-    ul.classList.toggle('show');
-    ul.previousElementSibling.classList.toggle('rotate');
-  })
+    Array.from(sBar.getElementsByClassName('show')).forEach(ul => {
+      ul.classList.remove('show');
+      ul.previousElementSibling.classList.remove('rotate');
+    })
+  }
 }
